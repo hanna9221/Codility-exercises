816a4fd99a43b734612a490381c27f30a7011d95
hanna9221 <42779417+hanna9221@users.noreply.github.com>
2019-03-08 13:32:22 +0800

    Update 20181007_Bromum 2018.py

--- Codility/20181007_Bromum 2018.py
+++ Codility/20181007_Bromum 2018.py
@@ -1,5 +1,4 @@
 # Bromum 2018
-# https://app.codility.com/cert/view/cert7N4E6K-A8V9RCXQ4UK6D7WS/
 
 def solution(N,Q,B,C):
     rec = {}
@@ -11,9 +10,3 @@ def solution(N,Q,B,C):
         if rec[(B[i], C[i])]==Q:
             return i
     return -1
-
-
-B = [1, 2, 0, 1, 1, 0, 0, 1]
-C = [0, 3, 0, 2, 0, 3, 0, 0]
-N, Q = 3, 2
-print(solution(N,Q,B,C))
